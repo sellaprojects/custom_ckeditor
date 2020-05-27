4e96ae010f1f02c3c4fdfc191efbf23766356d7b
Ali <sella@sellaprojects.com>
2020-05-28 06:42:15 +0900

    fixed the new paragraph bug

--- src/passagify.js
+++ src/passagify.js
@@ -29,67 +29,52 @@ export default class Passagify extends Plugin {
                     if(Number.isNaN(intTableWidth)) throw "Not a Number error";
                     editor.model.change( writer => {
                          const selection=editor.model.getSelectedContent(editor.model.document.selection);
-                         let selectedText= "";
+                         let selectedText= [];
                          for(let i=0; i<selection._children.length; i++){
                              const node=selection._children._nodes[i]
                              if(node.name==="paragraph"){
-                                selectedText=selectedText+node._children._nodes[0]._data+" ";
+                                selectedText.push(node._children._nodes[0]._data);
                              }else if(selection._children.length===1 && node._data){
-                                selectedText= node._data+" ";
+                                selectedText.push(node._data);
                                 
                              }
                              
                          }
 
-                        console.log(editor);
-                         
+
                         editor.model.deleteContent(editor.model.document.selection);
 
                         const tableUtils=new TableUtils();
-                        const charsPerRow = Math.floor(intTableWidth*60 / 300); 
-                        const rowsNum=Math.ceil(selectedText.length/charsPerRow);
-                        console.log("table width : " + intTableWidth);
-                        console.log("CharsPerRow: "+ charsPerRow);
-                        console.log("rowsNum: "+ rowsNum);
-                        const textArray= selectedText.split(" ");
-                        
-                        
-                        let start=0;
-
+                        const charsPerRow = Math.floor(intTableWidth*60 / 300);
+                        const rowsNum=this.getRowsNumber(selectedText,charsPerRow);                         
                         const table=tableUtils.createTable(writer,rowsNum,2);
-                        const firstCell1=table.getChild(0).getChild(0).getChild(0);
-                        const firstCell2=table.getChild(0).getChild(1).getChild(0);
-                        writer.insertText('Line',{ 'fontSize': 'small','fontColor': '#9b9b9d'  },firstCell1,0);
-                        writer.insertText("    "+this.getNextText(textArray,charsPerRow),firstCell2,0);
-                        
-                        for(let i=1;i<rowsNum;i++){
-                            start=start+charsPerRow;
-                            console.log("start: "+ start);
-                            
-                            const cell1 = table.getChild(i).getChild(0).getChild(0);
-                            const cell2 = table.getChild(i).getChild(1).getChild(0);
-                            const text=this.getNextText(textArray,charsPerRow)
-                            if((i+1)%5 === 0) {
-                                console.log(cell1);
-                                
-                                writer.insertText((i+1)+"",{ 'fontSize': 'small','fontColor': '#9b9b9d'  },cell1,0);
+                        let currentRow=0
+                        for(let pIdx=0;pIdx<selectedText.length;pIdx++){
+                            const parRowsNum = Math.ceil(selectedText[pIdx].length/charsPerRow);
+                            const textArray=selectedText[pIdx].split(" ");
+                            this.start=0;
+                            for(let i=0;i<parRowsNum;i++){
+                                const cell1 = table.getChild(currentRow).getChild(0).getChild(0);
+                                const cell2 = table.getChild(currentRow).getChild(1).getChild(0);
+                                const text=this.getNextText(textArray,charsPerRow);
+                                if(currentRow===3){
+                                    writer.insertText("Line",{ 'fontSize': 'small','fontColor': '#9b9b9d'  },cell1,0);
+                                }
+                                if((currentRow+1)%5 === 0) {
+                                   
+                                    writer.insertText((currentRow+1)+"",{ 'fontSize': 'small','fontColor': '#9b9b9d'  },cell1,0);
+                                }
+                                const spacer=currentRow==0?"    ":"";
+                                writer.insertText(spacer+text,cell2,0);
+                                currentRow++;
                             }
-                            
-                            console.log("INSERTING TEXT "+ text);
-                            
-                            writer.insertText(" "+text,cell2,0);
-                            
-                            
                         }
-                        console.log("setting the table");
-                        console.log(table);
-                        
+
                         editor.model.insertContent(table);
                     });
                 }catch(err){
                     console.log(err);
                 }
-               
                
             } );
 
@@ -112,5 +97,14 @@ export default class Passagify extends Plugin {
             }
         }
         return nexText
+    }
+
+    getRowsNumber(paragraphsArray,charsPerRow){
+        let count=0;
+        for(let paragraph of paragraphsArray){
+            count+=Math.ceil(paragraph.length/charsPerRow);
+        }
+        return count
+
     }
 }
