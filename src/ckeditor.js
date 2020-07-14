4e69b1acc803245c2bd2c4defc25155c74a621f7
Ali <sella@sellaprojects.com>
2020-07-14 14:49:35 +0900

    fixed indentation

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -21,6 +21,7 @@ import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -92,7 +93,8 @@ ClassicEditor.builtinPlugins = [
 	TextTransformation,
 	MathType,
 	Alignment,
-	Passagify
+	Passagify,
+	IndentBlock
 ];
 
 // Editor configuration.
